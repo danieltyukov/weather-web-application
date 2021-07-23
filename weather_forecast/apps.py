268abae0d68e38b310ed0722cd46f6f07a20962a
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-24 00:18:25 +0300

    doing migration fix ups

--- weather_forecast/apps.py
+++ weather_forecast/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WeatherForcastConfig(AppConfig):
+class WeatherForecastConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'weather_forcast'
+    name = 'weather_forecast'
